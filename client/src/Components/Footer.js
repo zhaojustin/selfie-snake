ea76ce6a9a90b58aa886368000297b4e88c59b1b
Justin Zhao <justin@justinzhao.net>
2024-06-13 13:34:04 -0700

    added download buttons

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,4 +1,4 @@
-import { HStack, Heading, Image, VStack, Text } from "@chakra-ui/react";
+import { HStack, Heading, Image, VStack, Text, Stack } from "@chakra-ui/react";
 import ReactGA from "react-ga4";
 const TRACKING_ID = "G-QJ3NR3QPZY"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
@@ -30,6 +30,36 @@ export const Footer = () => {
           tomotime
         </Heading>
       </HStack>
+
+      {/* download links */}
+      <Stack direction={["column", "row"]} my={5}>
+        <Image
+          src={"/download/app_store.png"}
+          maxW="120px"
+          _hover={{ cursor: "pointer" }}
+          onClick={() => {
+            ReactGA.event({
+              category: "redirect-to-tomotime",
+              action: "footer-click-app-store",
+            });
+            window.location.href =
+              "https://apps.apple.com/us/app/tomotime/id6446873602";
+          }}
+        />
+        <Image
+          src={"/download/google_play.png"}
+          maxW="120px"
+          _hover={{ cursor: "pointer" }}
+          onClick={() => {
+            ReactGA.event({
+              category: "redirect-to-tomotime",
+              action: "footer-click-play-store",
+            });
+            window.location.href =
+              "https://play.google.com/store/apps/details?id=com.tomotime&pcampaignid=web_share";
+          }}
+        />
+      </Stack>
     </VStack>
   );
 };

32f6f0aa01c41c2d5b62ded063b5bbd1828133d4
Justin Zhao <justin@justinzhao.net>
2024-06-13 12:46:23 -0700

    added ga

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,5 +1,19 @@
 import { HStack, Heading, Image, VStack, Text } from "@chakra-ui/react";
+import ReactGA from "react-ga";
+
+const useAnalyticsEventTracker = (category = "Blog category") => {
+  const eventTracker = (
+    action = "click on tomotime footer",
+    label = "tomotime footer click label"
+  ) => {
+    ReactGA.event({ category, action, label });
+  };
+  return eventTracker;
+};
+
 export const Footer = () => {
+  const gaEventTracker = useAnalyticsEventTracker("Contact us");
+
   return (
     <VStack spacing={2} pb={10}>
       <Text fontWeight="medium" size="md">
@@ -8,7 +22,10 @@ export const Footer = () => {
       <HStack
         spacing={2}
         _hover={{ cursor: "pointer" }}
-        onClick={() => window.location.assign("https://tomotime.app")}
+        onClick={() => {
+          window.location.assign("https://tomotime.app");
+          gaEventTracker("redirect-to-tomotime");
+        }}
       >
         <Image src="/icon.png" boxSize={8} borderRadius={10} />
         <Heading
